931bdd2b281fd4bc6ced660373793b6d49bbb49e
hugosenari <hugosenari@gmail.com>
2022-08-09 00:43:04 -0300

    feat(nixpkg): first commit

--- README.md
+++ README.md
@@ -7,5 +7,13 @@
 # Running
 
 ```sh
-nix run github:cruel-intentions/emelfl2-nixpkg
+nix run github:cruel-intentions/emelfm2-nixpkg
 ```
+
+# Know bugs
+
+Isn't stable
+
+# Special thanks to:
+
+Toar and [EmelFM2 developers](https://emelfm2.net/)
--- flake.nix
+++ flake.nix
@@ -4,15 +4,13 @@
   inputs.emelfm2.url   = "github:tom2tom/emelfm2";
   inputs.emelfm2.flake = false;
   outputs = inputs: 
-    let 
-      makeFlags = "WITH_KERNELFAM=1 WITH_GTK3=1 PREFIX=$out";
-      pkgs      = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    let pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     in
     {
       packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-        inherit makeFlags;
         buildInputs  = with pkgs; [file gtk3 pkg-config zlib];
         installPhase = "make PREFIX=$out install";
+        makeFlags    = "WITH_GTK3=1 PREFIX=$out";
         pname        = "emelfm2";
         src          = "${inputs.emelfm2}";
         version      = "0.9.1.2";
